1cc972fc2587e116d6c556cab5b0c3fcefc46178
dongsam-E <dong2dong32@gmail.com>
2023-07-11 18:07:46 +0900

    section bg 변경

--- src/component/Portfolio.js
+++ src/component/Portfolio.js
@@ -3,18 +3,21 @@ import portfolio from '../scss/portfolio.module.css';
 
 function Portfolio(props) {
     return (
-        <section id={props.id} className={`${portfolio.h}`}>
-            <div>
-                <div>
-                    <a href="">
-                        <img src="https://fourlab.co.kr/web/upload/appfiles/0zdpAngaKBFnlCcCqpCU4A/5d382ad289252f2ec0851dd542362c39.jpg" alt="dummyImg" />
-                    </a>
-                </div>
-                <div>
-                    <div>포폴종류ex Ongadam Website</div>
-                    <div>타이틀</div>
-                    <div>#스킬해시태그</div>
-                    <div><a href="">자세히보기</a></div>
+        <section id={props.id} className={`${portfolio.section}`}>
+            <div className={`py-5 ps-5`}>
+                <div className={`${portfolio.title} text-white`}>Portfolio</div>
+                <div className={`bg-white`}>
+                    <div>
+                        <a href="">
+                            <img src="https://fourlab.co.kr/web/upload/appfiles/0zdpAngaKBFnlCcCqpCU4A/5d382ad289252f2ec0851dd542362c39.jpg" alt="dummyImg" />
+                        </a>
+                    </div>
+                    <div>
+                        <div>포폴종류ex Ongadam Website</div>
+                        <div>타이틀</div>
+                        <div>#스킬해시태그</div>
+                        <div><a href="">자세히보기</a></div>
+                    </div>
                 </div>
             </div>
         </section>
