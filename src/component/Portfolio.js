8cdcc396da98cdb6b35d5de9a3242cdfacd5d028
dongsam-E <dong2dong32@gmail.com>
2023-07-11 17:58:09 +0900

    포트폴리오 구조 구상

--- src/component/Portfolio.js
+++ src/component/Portfolio.js
@@ -4,6 +4,19 @@ import portfolio from '../scss/portfolio.module.css';
 function Portfolio(props) {
     return (
         <section id={props.id} className={`${portfolio.h}`}>
+            <div>
+                <div>
+                    <a href="">
+                        <img src="https://fourlab.co.kr/web/upload/appfiles/0zdpAngaKBFnlCcCqpCU4A/5d382ad289252f2ec0851dd542362c39.jpg" alt="dummyImg" />
+                    </a>
+                </div>
+                <div>
+                    <div>포폴종류ex Ongadam Website</div>
+                    <div>타이틀</div>
+                    <div>#스킬해시태그</div>
+                    <div><a href="">자세히보기</a></div>
+                </div>
+            </div>
         </section>
     );
 }
