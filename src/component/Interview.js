37947f9bfe6531c4c4824abbb347b993a861028b
dongsam-E <dong2dong32@gmail.com>
2023-07-13 18:08:51 +0900

    인터뷰섹션 리액트 부트스트랩 컴포넌트 커스텀

--- src/component/Interview.js
+++ src/component/Interview.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import Accordion from 'react-bootstrap/Accordion';
 import interview from '../scss/interview.module.css';
 
 function Interview(props) {
     return (
-        <section id={props.id} className={`${interview.h}`}>
+        <section id={props.id} className={`${interview.sectionBg}`}>
+            <div className={`py-5 container`}>
+                <div className={`my-4 ${interview.name}`}>Interview</div>
+                <Accordion defaultActiveKey="0">
+                    {
+                        props.interviewdb.accordion.map((v, x) => {
+                            return(
+                                <>
+                                    <Accordion.Item eventKey={v.key} key={x}>
+                                        <Accordion.Header>{v.title}</Accordion.Header>
+                                        <Accordion.Body>
+                                            {v.con}
+                                        </Accordion.Body>
+                                    </Accordion.Item>
+                                </>
+                            )
+                        })
+                    }
+                </Accordion>
+            </div>
         </section>
     );
 }
---
+++ src/data/interview.js
@@ -0,0 +1,21 @@
+const interviewDB = {
+    accordion : [
+        {
+            title : "사전인터뷰 타이틀0"
+            ,con : "내용입니다."
+            ,key : "0"
+        }
+        ,{
+            title : "사전인터뷰 타이틀1"
+            ,con : "내용입니다."
+            ,key : "1"
+        }
+        ,{
+            title : "사전인터뷰 타이틀2"
+            ,con : "내용입니다."
+            ,key : "2"
+        }
+    ]
+}
+
+export default interviewDB
