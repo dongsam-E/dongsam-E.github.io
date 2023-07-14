6996a77471758b2e43d39acc089098eca786c7da
dongsam-E <dong2dong32@gmail.com>
2023-07-14 16:07:43 +0900

    폼디자인 변경

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -3,6 +3,8 @@ import contact from '../scss/contact.module.css';
 
 function Contact(props) {
 
+    
+
     return (
         <section id={props.id}>
             <div className={`py-5 container`}>
@@ -10,12 +12,12 @@ function Contact(props) {
                 <div className={`${contact.sub}`}><p className={`mb-3`}>제안 직무를 선택해주세요</p></div>
                 <div>
                     <form action="" method="POST" name="contact" id={`${contact.form}`}>
-                        <ul id="subform" className={`${contact.checkbox} p-0`}>
+                        <ul id="subform" className={`${contact.checkbox} p-0 row`}>
                             {
                                 props.contactdb.checkbox.map((v, x) =>{
                                     return(
-                                        <li key={x} className={``}>
-                                            <label>
+                                        <li key={x} className={`py-1 col-6 col-md-3 d-flex`}>
+                                            <label className={`${contact.checkboxlist}`}>
                                                 <input
                                                     type="checkbox"
                                                     name={v.name}
@@ -35,21 +37,35 @@ function Contact(props) {
                                 {
                                     props.contactdb.data.map((v, x) =>{
                                         return(
-                                            <li className={`${contact.formsub} ${v.liCls}`}>
+                                            <li key={x} className={`${contact.formsub}`}>
                                                 <input
-                                                    key={x}
-                                                    className={`${v.inputCls} ${contact.inputCls}`}
+                                                    className={`${v.inputCls} ${contact.mainCls}`}
                                                     type={v.type}
                                                     placeholder={v.placeholder}
-                                                    value={v.value}
-                                                    id={v.id}
+                                                    name={v.name}
                                                 />
-                                                <label for={v.for} className={v.labelCls}>{v.label}</label>
                                             </li>
                                         )
                                     })
                                 }
                         </ul>
+                        <div id={`${contact.submit}`}>
+                            {
+                                props.contactdb.submit.map((v, x) =>{
+                                    return(
+                                        <div key={x} className = {`${contact.formsub} ${v.liCls}`}>
+                                            <input 
+                                                className={`${v.inputCls} ${contact.footerCls}`}
+                                                type={v.type}
+                                                value={v.value}
+                                                id={v.id}
+                                            />
+                                            <label for={v.for} className={v.labelCls}>{v.label}</label>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
                     </form>
                 </div>
             </div>
--- src/data/contact.js
+++ src/data/contact.js
@@ -1,19 +1,19 @@
 const contactDB = {
     checkbox: [
         {
-            name: "radio0"
+            name: "checkbox0"
             ,title: "프론트엔드"
         }
         ,{
-            name: "radio1"
+            name: "checkbox1"
             ,title: "퍼블리셔"
         }
         ,{
-            name: "radio2"
+            name: "checkbox2"
             ,title: "기획/마케팅"
         }
         ,{
-            name: "radio3"
+            name: "checkbox3"
             ,title: "디자인"
         }
     ]
@@ -21,48 +21,40 @@ const contactDB = {
         {
             type: "text"
             ,placeholder: "회사 명"
-            ,liCls: ""
             ,inputCls: "w-100"
-            ,value: ""
         }
         ,{
             type: "text"
             ,placeholder: "담당자 명"
-            ,liCls: ""
             ,inputCls: "w-100"
-            ,value: ""
         }
         ,{
             type: "tel"
             ,placeholder: "대표 번호"
-            ,liCls: ""
             ,inputCls: "w-100"
-            ,value: ""
         }
         ,{
             type: "text"
             ,placeholder: "제안 내용"
-            ,liCls: ""
             ,inputCls: "w-100"
-            ,value : ""
         }
-        ,{
+    ]
+    ,submit :[
+        , {
             type: "checkbox"
-            ,placeholder: ""
-            ,liCls: "d-flex justify-content-end py-3"
-            ,inputCls: ""
-            ,id: "policy"
-            ,value: "policy"
-            ,for: "policy"
-            ,label: "개인정보정책 약관 동의"
-            ,labelCls: "ps-3"
+            , liCls: "d-flex justify-content-end py-3"
+            , inputCls: ""
+            , id: "policy"
+            , value: "policy"
+            , for: "policy"
+            , label: "개인정보정책 약관 동의"
+            , labelCls: "d-flex align-items-center"
         }
-        ,{
+        , {
             type: "submit"
-            ,placeholder: ""
-            ,liCls: ""
-            ,inputCls: "w-100"
-            ,value : "면접 제안하기"
+            , liCls: ""
+            , inputCls: "w-100"
+            , value: "면접 제안하기"
         }
     ]
 }
