a3c8b341fef288971d127c3dd94b174a3b7bb41b
dongsam-E <dong2dong32@gmail.com>
2023-07-26 18:12:09 +0900

    콘텍트섹션 useState 기능 추가

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,9 +1,41 @@
-import React, {useState ,useEffect, useRef} from "react";
+import React, {useState ,useEffect, useRef, useCallback} from "react";
 import contact from '../scss/contact.module.css';
 import emailjs from '@emailjs/browser';
+import { Value } from "sass";
 
 
 function Contact(props) {
+    
+    const [company, setCompany] = useState("");
+    const [name, setName] = useState("");
+    const [num, setNum] = useState("");
+    const [text, setText] = useState("");
+    const [isFormValid, setIsFormValid] = useState(false);
+
+    const handleCompanyChange = (e) => {
+        setCompany(e.target.value);
+    }
+    const handleNameChange = (e) => {
+        setName(e.target.value);
+    }
+    const handleNumChange = (e) => {
+        setNum(e.target.value);
+    }
+    const handleTextChange = (e) => {
+        setText(e.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+            if (company !== '' && name !== '' && num !== '' && text !== ''){
+                alert('메일이 전송되었습니다. 빠른 회신 드리겠습니다!')
+            }else{
+                alert('모든 필수 입력값을 채워주세요.')
+            }
+    }
+
+
+
     const form = useRef();
 
     const sendEmail = (e) => {
@@ -24,7 +56,7 @@ function Contact(props) {
                 <h2 className={`my-4 ${contact.name}`}>Contact ME!</h2>
                 <div className={`${contact.sub}`}><p className={`mb-3`}>제안 직무를 선택해주세요</p></div>
                 <div>
-                    <form ref={form} onSubmit={sendEmail} name="contact" id={`${contact.form}`}>
+                    <form ref={form} onSubmit={sendEmail && handleSubmit} name="contact" id={`${contact.form}`}>
                         <ul id="subform" className={`${contact.checkbox} p-0 row`}>
                             {
                                 props.info.contactdb.checkbox.map((v, x) =>{
@@ -47,37 +79,58 @@ function Contact(props) {
                             }
                         </ul>
                         <ul id={`${contact.mainform}`} className={`p-0 m-0`}>
-                                {
-                                    props.info.contactdb.data.map((v, x) =>{
-                                        return(
-                                            <li key={x} className={`${contact.formsub}`}>
-                                                <input
-                                                    className={`${v.inputCls} ${contact.mainCls}`}
-                                                    type={v.type}
-                                                    placeholder={v.placeholder}
-                                                    name={v.name}
-                                                />
-                                            </li>
-                                        )
-                                    })
-                                }
+                            <li className={`${contact.formsub}`}>
+                                <input
+                                    className={`w-100 ${contact.mainCls}`}
+                                    type="text"
+                                    placeholder="회사 명"
+                                    name="company"
+                                    onChange={handleCompanyChange}
+                                    value={company}
+                                    required
+                                />
+                            </li>
+                            <li className={`${contact.formsub}`}>
+                                <input
+                                    className={`w-100 ${contact.mainCls}`}
+                                    type="text"
+                                    placeholder="담당자 명"
+                                    name="name"
+                                    onChange={handleNameChange}
+                                    value={name}
+                                    required
+                                />
+                            </li>
+                            <li className={`${contact.formsub}`}>
+                                <input
+                                    className={`w-100 ${contact.mainCls}`}
+                                    type="tel"
+                                    placeholder="대표 번호"
+                                    name="num"
+                                    onChange={handleNumChange}
+                                    value={num}
+                                    required
+                                />
+                            </li>
+                            <li className={`${contact.formsub}`}>
+                                <input
+                                    className={`w-100 ${contact.mainCls}`}
+                                    type="text"
+                                    placeholder="제안 내용"
+                                    name="text"
+                                    onChange={handleTextChange}
+                                    value={text}
+                                    required
+                                />
+                            </li>
                         </ul>
-                        <div id={`${contact.submit}`}>
-                            {
-                                props.info.contactdb.submit.map((v, x) =>{
-                                    return(
-                                        <div key={x} className = {`${contact.formsub} ${v.liCls}`}>
-                                            <input 
-                                                className={`${v.inputCls} ${contact.footerCls}`}
-                                                type={v.type}
-                                                value={v.value}
-                                                id={v.id}
-                                            />
-                                            <label for={v.for} className={v.labelCls}>{v.label}</label>
-                                        </div>
-                                    )
-                                })
-                            }
+                        <div id={`submit`} className={`${contact.formsub} d-flex justify-content-end py-3`}>
+                            <input
+                                className={`w-100 ${isFormValid ? contact.Active : contact.Disabled}`}
+                                type="submit"
+                                value="면접 제안하기"
+                                disabled={company.length<1 || name.length<1 || num.length<1 || text.length<1}
+                            />
                         </div>
                     </form>
                 </div>
