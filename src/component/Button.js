5a1fd566faf514ad7d4abe82953f59e6270cfc80
dongsam-E <dong2dong32@gmail.com>
2023-07-31 18:09:01 +0900

    최상단버튼 컴포넌트 추가
    스타일 / 기능 적용

---
+++ src/component/Button.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+import button from '../scss/button.module.css'
+
+function Button(props) {
+    const [toggleBtn, setToggleBtn] = useState(true);
+
+    const handleScroll = () => {
+        const {scrollY} = window;
+
+        scrollY > 10 ? setToggleBtn(true) : setToggleBtn(false);
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    const goToTop = () => {
+        window.scrollTo({top : 0, behavior : "smooth"});
+    };
+
+    return toggleBtn ? (
+        <div id={props.id} onClick={goToTop} className={`${button.bg}`}>
+            <i class={`bi bi-chevron-up ${button.arrow}`}></i>
+            <div className={`${button.main}`}>
+                <span>TOP</span>
+            </div>
+        </div>
+    ) : null;
+}
+
+export default Button
--- src/layout/Content.js
+++ src/layout/Content.js
@@ -4,6 +4,7 @@ import Portfolio from '../component/Portfolio'
 import Interview from '../component/Interview'
 import Contact from '../component/Contact'
 import Insta from '../component/Insta'
+import Button from '../component/Button'
 
 function Content(props) {
     return (
@@ -14,6 +15,7 @@ function Content(props) {
             <Interview id="interview" info={props.info}></Interview>
             <Contact id="contact" info={props.info}></Contact>
             <Insta id="insta" info={props.info}></Insta>
+            <Button id="button"></Button>
         </div>
     )
 }
