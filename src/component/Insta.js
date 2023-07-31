14c548867b49429173fa2f8310cc5f4400c3fb21
dongsam-E <dong2dong32@gmail.com>
2023-07-31 12:58:55 +0900

    불필요컨텐츠 삭제

--- src/component/Insta.js
+++ src/component/Insta.js
@@ -7,36 +7,7 @@ function Insta(props) {
   
   return (
     <section id={props.id}>
-          <div className={`${insta.slidebox}`}>
-            <div className={`d-flex ${insta.gallery}`}>
-                <ul className={`d-flex ${insta.slidelist} ${insta.original}`}>
-                  {
-                    props.info.instadb.img.map((v, i) => {
-                      return(
-                          <li>
-                              <a href="">
-                                <img src={v.src} alt="" className={`${insta.img}`} />
-                              </a>
-                          </li>
-                      )
-                    })
-                  }
-                </ul>
-                <ul className={`d-flex ${insta.slidelist} ${insta.clone}`}>
-                  {
-                    props.info.instadb.img.map((v, i) => {
-                      return(
-                          <li>
-                              <a href="">
-                                <img src={v.src} alt="" className={`${insta.img}`} />
-                              </a>
-                          </li>
-                      )
-                    })
-                  }
-                </ul>
-            </div>
-          </div>
+          
     </section>
   )
 }
