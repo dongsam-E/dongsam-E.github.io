79bbc9ad3a7c4441cc1755eeb15e89f2d4749314
dongsam-E <dong2dong32@gmail.com>
2023-07-31 18:08:22 +0900

    인스타 섹션 반응형처리 / 이미지 삽입

--- src/component/Insta.js
+++ src/component/Insta.js
@@ -4,10 +4,49 @@ import insta from '../scss/insta.module.css';
 
 
 function Insta(props) {
-  
+
   return (
-    <section id={props.id}>
-          
+    <section id={props.id} className={`${insta.section}`}>
+      <div className={`${insta.bgimg}`}>
+        <img src="./img/snsimg.png" alt="snsimg" />
+      </div>
+      <div>
+          <div className={`${insta.box}`}>
+              <div className={`d-flex ${insta.topimg}`}>
+                  <div className={`${insta.text}`}>
+                      <a href=""><span>@dongsam-e</span></a>
+                      <p>INSTAGRAM</p>
+                  </div>
+                  <div>
+                      <a href="">
+                        <img src="./img/insta1.jpg" alt="" />
+                      </a>
+                  </div>
+                  <div>
+                      <a href="">
+                        <img src="./img/insta2.jpg" alt="" />
+                      </a>
+                  </div>
+              </div>
+              <div className={`d-flex ${insta.bottomimg}`}>
+                  <div>
+                      <a href="">
+                        <img src="./img/insta3.jpg" alt="" />
+                      </a>
+                  </div>
+                  <div>
+                      <a href="">
+                        <img src="./img/insta4.jpg" alt="" />
+                      </a>
+                  </div>
+                  <div>
+                      <a href="">
+                        <img src="./img/insta5.jpg" alt="" />
+                      </a>
+                  </div>
+              </div>
+          </div>
+      </div>
     </section>
   )
 }
