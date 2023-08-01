5ae50285dc7401baa34592a6f5e98dd621fe514c
dongsam-E <dong2dong32@gmail.com>
2023-08-01 17:56:22 +0900

    인스타url삽입

--- src/component/Insta.js
+++ src/component/Insta.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import insta from '../scss/insta.module.css';
 
-
+//json에 데이터넣어서 맵 돌려야함
 
 function Insta(props) {
 
@@ -14,33 +14,33 @@ function Insta(props) {
           <div className={`${insta.box}`}>
               <div className={`d-flex ${insta.topimg}`}>
                   <div className={`${insta.text}`}>
-                      <a href=""><span>@dongsam-e</span></a>
+                      <a href="https://www.instagram.com/dongsam_e/"  target="_blank"><span>@dongsam-e</span></a>
                       <p>INSTAGRAM</p>
                   </div>
                   <div>
-                      <a href="">
+                      <a href="" target="_blank">
                         <img src="./img/insta1.jpg" alt="" />
                       </a>
                   </div>
                   <div>
-                      <a href="">
+                      <a href="https://www.instagram.com/p/CvWDIGdBXIB/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA==" target="_blank">
                         <img src="./img/insta2.jpg" alt="" />
                       </a>
                   </div>
               </div>
               <div className={`d-flex ${insta.bottomimg}`}>
                   <div>
-                      <a href="">
+                      <a href="" target="_blank">
                         <img src="./img/insta3.jpg" alt="" />
                       </a>
                   </div>
                   <div>
-                      <a href="">
+                      <a href="" target="_blank">
                         <img src="./img/insta4.jpg" alt="" />
                       </a>
                   </div>
                   <div>
-                      <a href="">
+                      <a href="" target="_blank">
                         <img src="./img/insta5.jpg" alt="" />
                       </a>
                   </div>
