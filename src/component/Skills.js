850796fbddee9ca2160cec083f501feb29c14bad
dongsam-E <dong2dong32@gmail.com>
2023-09-19 18:12:35 +0900

    스킬섹션 막대그래프로 수정

--- src/component/Skills.js
+++ src/component/Skills.js
@@ -1,23 +1,93 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import skills from '../scss/skills.module.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 
 function Skills(props) {
-
+    useEffect(() => {
+        AOS.init();
+    })
+    
     return (
-        <section id={props.id} className={`${skills.sectionBg}`}>
+        <section id={props.id} className={`${skills.sectionBg} `}>
             <h2 className={`my-4 ${skills.name}`}>My Skills</h2>
-            <div className={`container`}>
-                <div className={`${skills.con}`}>
-                    {
-                        props.info.skillsdb.skills.map((v,x) => {
-                            return(
-                                <div key={x} className={`bg-white mx-3 my-2 ${v.boxCls} ${skills.box} ${skills.ani}`}>
-                                    <h2 className={`${skills.title}`}>{v.title}</h2>
-                                    <img src={v.img} alt="" className={`${v.imgcls}`} />
-                                </div>
-                            )
-                        })
-                    }
+            <div className={`anibox container d-flex flex-column align-items-center`} data-aos="">
+                <div class="perbox">
+                    <h3>FrontEnd</h3>
+                    <div>
+                        {
+                            props.info.skillsdb.FrontEnd.map((v, i) => {
+                                return(
+                                    <div className={`imgbox frontimgbox ${v.title}`}>
+                                        <img src={v.img} alt="logo" key={i} />
+                                        <div className={`${v.graph} mx-4`}></div>
+                                        <span>{v.percent}</span>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+                <div class="perbox">
+                    <h3>BackEnd</h3>
+                    <div>
+                        {
+                            props.info.skillsdb.BackEnd.map((v, i) => {
+                                return(
+                                    <div className={`imgbox backimgbox ${v.title}`}>
+                                        <img src={v.img} alt="logo" key={i} />
+                                        <div className={`${v.graph} mx-4`}></div>
+                                        <span>{v.percent}</span>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+                <div class="perbox">
+                    <h3>Graphic</h3>
+                    <div>
+                        {
+                            props.info.skillsdb.Etc.map((v, i) => {
+                                return (
+                                    <div className={`imgbox etcimgbox ${v.title}`}>
+                                        <img src={v.img} alt="logo" key={i} />
+                                        <div className={`${v.graph} mx-4`}></div>
+                                        <span>{v.percent}</span>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+                <div class="perbox pt-3">
+                    <h3>프로젝트 보고 및 <br /> 협업 가능한 커뮤니케이션 툴</h3>
+                    <div className="d-flex align-items-center justify-content-center">
+                        {
+                            props.info.skillsdb.Commu.map((v, i) => {
+                                return(
+                                    <div className={`imgbox commuimgbox px-3 ${v.title}`}>
+                                        <img src={v.img} alt="logo" key={i} />
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+                <div class="perbox pt-3">
+                    <h3>보유 자격증</h3>
+                    <div className="d-flex align-items-center justify-content-center">
+                        {
+                            props.info.skillsdb.Certi.map((v, i) => {
+                                return(
+                                    <div className="imgbox certiimgbox px-3">
+                                        <img src={v.img} alt="logo" key={i} />
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
                 </div>
             </div>
         </section>
