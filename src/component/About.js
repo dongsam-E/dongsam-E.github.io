ee7bafff507a27d61a36174677da33b7d88ac104
dongsam-E <dong2dong32@gmail.com>
2023-09-26 15:26:26 +0900

    선주현배너이미지 스와이퍼완성

--- src/component/About.js
+++ src/component/About.js
@@ -1,49 +1,62 @@
 import React, { useRef, useState } from 'react';
+import about from '../scss/about.module.css';
+
 import { Swiper, SwiperSlide } from 'swiper/react';
 import "swiper/css";
-import { Autoplay } from 'swiper';
-import about from '../scss/about.module.css';
+import 'swiper/css/effect-fade';
+import { EffectFade, Autoplay } from 'swiper';
 
 function About(props) {
     return (
         <section id={props.id} className={`${about.section}`}>
             <div id={`${about.box}`} className={`container`}>
-                {/* <div id={`${about.imgSection}`}>
+                <div className='imgSection'>
                     <Swiper
-                        modules={[Autoplay]}
+                        modules={[EffectFade, Autoplay]}
                         slidesPerView={1}
                         loop={true}
                         autoplay={{
                             delay: 3000,
                             disableOnInteraction: false,
                         }}
+                        effect={'fade'}
+                        fadeEffect={{
+                            crossFade: true
+                        }}
                         className="mySwiper d-flex align-items-center"
                     >
                         <SwiperSlide>
-                            <img src="./img/sunjuhyeon.png" alt="aboutimg" />
+                            <div className='aboutbg0'></div>
                         </SwiperSlide>
                         <SwiperSlide>
-                            <img src="./img/aboutimgfade.jpeg" alt="aboutimg" />
+                            <div className='aboutbg1'></div>
                         </SwiperSlide>
                     </Swiper>
-                </div> */}
-                <div id={`${about.script}`} className={`d-flex flex-column align-items-center justify-content-center col-lg-6`}>
+                </div>
+                <div id={`${about.script}`} className={`d-flex flex-column align-items-center justify-content-center col-lg-6 ${about.doubleQuote}`}>
                     {/* <img className={`${about.icon}`} src="./img/doubleQuote0.png" alt="icon" /> */}
                     <div className={`${about.main} text-center`}>
                         <br /><div className={`m-0 ${about.highlight}`}>
 
-                            <p className={`m-0 ${about.text1} position-relative`}><span className={`position-relative ${about.mark}`}>효율적인 서치능력,</span> <span className={`position-relative ${about.mark}`}>꾸준한 기록,</span><span className={`position-relative ${about.mark}`}>신속한 속도</span></p>
+                            <p className={`m-0 ${about.text1} position-relative`}><span className={`position-relative ${about.mark}`}>효율적인 서치능력,</span> <span className={`position-relative ${about.mark}`}>꾸준한 기록,</span> <span className={`position-relative ${about.mark}`}>신속한 속도</span></p>
                         </div>
                         <p className={`m-0 ${about.text0}`}>그리고 <span className={`${about.nonetext}`}>열린마음으로</span> <em className={`${about.em}`}>새로운 아이디어를 환영</em>하는 개발자</p>
                         <br /><p className={`m-0 ${about.text2}`}>선주현</p>
                     </div>
                     <div className={`${about.sub} d-flex flex-column text-center`}>
-                        <p className={`m-0`}><i class="bi bi-caret-right-fill"></i>생년월일 : <span>97.12.15</span></p>
-                        <p className={`m-0`}><i class="bi bi-caret-right-fill"></i>학력사항 : <span>숙명여자대학교(졸)</span></p>
+                        <p className={`m-0 d-flex align-items-center`}>
+                            {/* <i class="bi bi-caret-right-fill"></i>생년월일 :  */}
+                            <span className='afterdeco d-flex align-items-center'>97.12.15</span>
+                            <span>숙명여자대학교(졸)</span>
+                        </p>
+                        {/* <p className={`m-0`}>
+                            <i class="bi bi-caret-right-fill"></i>
+                        학력사항 : 
+                        </p> */}
                     </div>
                 </div>
             </div>
-        </section>
+        </section >
     );
 }
 
